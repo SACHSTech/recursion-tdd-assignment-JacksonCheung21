eff719282b0fa04c99b44d86da7d4160e1e985ba
Jackson Cheung <>
2021-05-16 03:02:34 +0000

    Tdd-Refactor count7

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -4,8 +4,20 @@ public class Recursion{
 
   public static int count7(int n){
     
-    return 2;
+    // Base Case
+    if (7 > n) {
+      return 0;
+    }   
 
+    // If the digit at the end is a 7 it would return 1 + the amount of 7 in the number
+    else if (n % 10 == 7) {
+      return 1 + count7(n / 10);
+    }
+
+    // If the digit at the end is not a 7 it would return the amount of 7 in the number
+    else {
+      return count7(n / 10);
+    }
 
   }
 
--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -6,25 +6,43 @@ import org.junit.After;
 
 import recursionAssignment.Recursion;
 
+/**
+* Test file for Recursion.java
+* @author: J.Cheung
+*
+*/
+
 public class RecursionTest{
-    
-
-  /**
-   * An initial test
-  */
   
+  @Before
+  public void beforeTest(){
+    // some steps to peform before tests are run
+  }
 
-   @After 
+  @After 
   public void afterTest(){
     // some steps to perform after tests are run
   }
+  
+  /**
+   * An initial test
+  */
  
+  // Problem set 1 - count7
+
   @Test
   public void Test1(){
-    assertEquals(Recursion.count7(717),2);
+    assertEquals(2,Recursion.count7(717));
   }
   
- 
-    
+  @Test
+  public void Test2(){
+    assertEquals(1,Recursion.count7(7));
+  }
+  
+  @Test
+  public void Test3(){
+    assertEquals(0,Recursion.count7(123));
+  }
     
 }
